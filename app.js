7202e86a492add2b152236917297c348f4ca477b
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-23 16:43:14 -0500

    Fix constant prompting. Remove container.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let btn = document.getElementById("new");
+btn.addEventListener("click", newGrid);
+
 function newGrid() {
     // Ask for the size.
     let size = prompt("Please enter your grid dimensions: ");
@@ -6,12 +9,16 @@ function newGrid() {
         let size = prompt("Sorry, the maximum dimension is 100! Please enter a new dimension: ");
     }
 
+    // Remove the previous grid.
+    let removeMe = document.getElementById("contain");
+    document.body.removeChild(removeMe);
     createGrid(size);
 }
 
 function createGrid(size) {
     let container = document.createElement("div"); // Create container for divs
     container.classList.add("container");
+    container.setAttribute('id', 'contain');
 
     // Create 64 "squares" and place them in the container.
     // Will use CSS to create the "grid" effect.
@@ -33,8 +40,4 @@ function createGrid(size) {
 
 function startUp() {
     createGrid(16);
-}   
-
-
-let button = document.getElementsByClassName("newGrid");
-button.addEventListener("click", newGrid());
+}
