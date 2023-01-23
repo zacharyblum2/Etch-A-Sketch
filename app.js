051ce987140a0d9cab345e4dbcce24a57f3c000f
zacharyblum2 <blum.zachary1@gmail.com>
2023-01-23 16:27:07 -0500

    Create function to ask for size of new grid. Turn previous grid code into function createGrid.

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
-// Set up a 16x16 grid of square divs. 
-// Have a container hold the 16x16 divs, and place it in the HTML.
+function newGrid() {
+    // Ask for the size.
+    let size = prompt("Please enter your grid dimensions: ");
 
-let container = document.createElement("div"); // Create container for divs
-container.classList.add("container");
+    while (size > 100) {
+        let size = prompt("Sorry, the maximum dimension is 100! Please enter a new dimension: ");
+    }
 
-// Create 64 "squares" and place them in the container.
-// Will use CSS to create the "grid" effect.
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-
-    // When you hover over a square, it changes its background color.
-    square.addEventListener("mouseover", event => {
-        square.style.backgroundColor = "black";
-    });
-
-    container.appendChild(square);
+    createGrid(size);
 }
 
-// Append the container to the body. 
-document.body.appendChild(container);
+function createGrid(size) {
+    let container = document.createElement("div"); // Create container for divs
+    container.classList.add("container");
+
+    // Create 64 "squares" and place them in the container.
+    // Will use CSS to create the "grid" effect.
+    for (let i = 0; i < (size * size); i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+
+        // When you hover over a square, it changes its background color.
+        square.addEventListener("mouseover", event => {
+            square.style.backgroundColor = "black";
+        });
+
+        container.appendChild(square);
+    }
+
+    // Append the container to the body. 
+    document.body.appendChild(container);
+}
+
+function startUp() {
+    createGrid(16);
+}   
+
+
+let button = document.getElementsByClassName("newGrid");
+button.addEventListener("click", newGrid());
